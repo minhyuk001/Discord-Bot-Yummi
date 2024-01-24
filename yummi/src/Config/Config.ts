e86d8eb672e733d3f7f1ca50154e704e7c9d0c6d
이민석 <115360167+leeminseok07@users.noreply.github.com>
2024-01-24 18:05:32 +0900

    Update Config.ts

--- yummi/src/Config/Config.ts
+++ yummi/src/Config/Config.ts
@@ -1,1 +1,1 @@
-export const TOKEN: string = process.env.TOKEN || "MTE5OTU0MDA3ODg5NzI3NDkwMA.GwpniG.hZK16W-ZFA5DwVl_JwLd5wiqWvYL7ESGmOiCqc";
+export const TOKEN: string = process.env.TOKEN || "token";
